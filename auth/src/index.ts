8761a6597e29063fa52d175d018db0c8b0315754
matus <matusdubrava@gmail.com>
2020-08-06 17:50:40 +0200

    added mongodb + connection initialization

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,10 @@
+import mongoose from 'mongoose';
+
 import { app } from './app';
 
 const PORT = process.env.PORT;
 const API_VERSION = process.env.API_VERSION;
+const MONGO_URI = process.env.MONGO_URI;
 
 if (!PORT) {
 	throw new Error('PORT is not set');
@@ -11,6 +14,21 @@ if (!API_VERSION) {
 	throw new Error('API_VERSION is not set');
 }
 
-app.listen(PORT, () => {
+if (!MONGO_URI) {
+	throw new Error('MONGO_URI is not set');
+}
+
+app.listen(PORT, async () => {
+	try {
+		await mongoose.connect(`${MONGO_URI}/users`, {
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useNewUrlParser: true,
+		});
+		console.log('successfully connected to database');
+	} catch (err) {
+		throw new Error('ERROR: failed to connect to database');
+	}
+
 	console.log(`auth service listening on port ${PORT}`);
 });
