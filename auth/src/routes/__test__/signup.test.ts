11c8b2d1320c83b3b683081a8f4ba2cc6f96e550
matus <matusdubrava@gmail.com>
2020-08-16 16:09:25 +0200

    implemented basic tests for signup route and removed console log from middleware

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,110 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+const API_VERSION = process.env.API_VERSION;
+const signupPostUrl = `/api/${API_VERSION}/auth/signup`;
+
+const testEmail = 'test@test.com';
+const invalidTestEmail = 'test.com';
+const testPassword = 'testpassword12345';
+const invalidTestPassword = '12345'; // too short, required at least 8 characters
+const testName = 'testname';
+
+it('returns 201 on successful signup', async () => {
+	return request(app)
+		.post(signupPostUrl)
+		.send({
+			email: testEmail,
+			name: testName,
+			password: testPassword,
+		})
+		.expect(201);
+});
+
+it('should return 201 even when the same name is used by more than one user', async () => {
+	await request(app)
+		.post(signupPostUrl)
+		.send({
+			email: testEmail,
+			name: testName,
+			password: testPassword,
+		})
+		.expect(201);
+
+	// change only email (which needs to be unique)
+	await request(app)
+		.post(signupPostUrl)
+		.send({
+			email: `test@test.io`,
+			name: testName,
+			password: testPassword,
+		})
+		.expect(201);
+});
+
+it('should fail with 422 if any of the required fields is missing [email, name, and password]', async () => {
+	await request(app)
+		.post(signupPostUrl)
+		.send({
+			email: testEmail,
+			name: testName,
+		})
+		.expect(422);
+
+	await request(app)
+		.post(signupPostUrl)
+		.send({
+			email: testEmail,
+			password: testPassword,
+		})
+		.expect(422);
+
+	await request(app)
+		.post(signupPostUrl)
+		.send({
+			password: testPassword,
+			name: testName,
+		})
+		.expect(422);
+});
+
+it('should fail with 422 if invalid email address is supplied', async () => {
+	await request(app)
+		.post(signupPostUrl)
+		.send({
+			password: testPassword,
+			email: invalidTestEmail,
+		})
+		.expect(422);
+});
+
+it('should fail with 422 if password with less than 8 characters is used', async () => {
+	await request(app)
+		.post(signupPostUrl)
+		.send({
+			password: invalidTestPassword,
+			name: testName,
+			email: testEmail,
+		})
+		.expect(422);
+});
+
+it('should fail with 422 if email address is already in use', async () => {
+	await request(app)
+		.post(signupPostUrl)
+		.send({
+			password: testPassword,
+			name: testName,
+			email: testEmail,
+		})
+		.expect(201);
+
+	await request(app)
+		.post(signupPostUrl)
+		.send({
+			password: testPassword,
+			name: testName,
+			email: testEmail,
+		})
+		.expect(422);
+});
