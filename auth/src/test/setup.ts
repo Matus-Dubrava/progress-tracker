bf846c6b612f5e4dc90542042b40fa9e401f3b02
matus <matusdubrava@gmail.com>
2020-08-16 15:44:59 +0200

    added test setup configuration

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,33 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+let mongo: any;
+
+beforeAll(async () => {
+	process.env.JWT_KEY = 'testkey12345';
+
+	mongo = new MongoMemoryServer();
+	const mongoURI = await mongo.getUri();
+
+	await mongoose.connect(mongoURI, {
+		useCreateIndex: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+});
+
+// delete every collection before each test
+beforeEach(async () => {
+	const collections = await mongoose.connection.db.collections();
+
+	for (let collection of collections) {
+		await collection.deleteMany({});
+	}
+});
+
+// stop mongo memory server and close mongoose connection
+// all test are done
+afterAll(async () => {
+	await mongo.stop();
+	await mongoose.connection.close();
+});
