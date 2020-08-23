0119039fe9bd8d8961201915536f114d6c4d64fb
matus <matusdubrava@gmail.com>
2020-08-23 21:54:45 +0200

    implemented project items fetching route

--- backend/src/routes/project-items/__test__/fetch-project-items.test.ts
+++ backend/src/routes/project-items/__test__/fetch-project-items.test.ts
@@ -13,7 +13,8 @@ let userAId: string;
 let userBId: string;
 let cookieUserA: string;
 let cookieUserB: string;
-let projectId: string;
+let projectIdUserA: string;
+let projectIdUserB: string;
 
 beforeEach(async () => {
 	let response = await request(app)
@@ -41,53 +42,86 @@ beforeEach(async () => {
 	cookieUserB = parseCookieFromResponse(response);
 
 	response = await createProject(cookieUserA, projectConfig.testProjectName1);
-	projectId = response.body.id;
+	projectIdUserA = response.body.id;
+
+	response = await createProject(cookieUserB, projectConfig.testProjectName2);
+	projectIdUserB = response.body.id;
 });
 
 it('should return all project items if no query string param is set', async () => {
 	await createProjectItem(
 		cookieUserA,
 		projectItemConfig.categoryIssue,
-		projectId
+		projectIdUserA
 	);
 	await createProjectItem(
 		cookieUserA,
 		projectItemConfig.categoryIssue,
-		projectId
+		projectIdUserA
 	);
 	await createProjectItem(
 		cookieUserA,
 		projectItemConfig.categoryTask,
-		projectId
+		projectIdUserA
 	);
 
 	const response = await request(app)
-		.get(`${projectConfig.baseProjectUrl}/${projectId}/items`)
+		.get(`${projectConfig.baseProjectUrl}/${projectIdUserA}/items`)
 		.set('Cookie', cookieUserA)
 		.expect(200);
 
 	expect(response.body.length).toEqual(3);
 });
 
+it('should return properly serialized project items', async () => {
+	let response = await createProjectItem(
+		cookieUserA,
+		projectItemConfig.categoryTask,
+		projectIdUserA
+	);
+
+	response = await request(app)
+		.get(`${projectConfig.baseProjectUrl}/${projectIdUserA}/items`)
+		.set('Cookie', cookieUserA)
+		.expect(200);
+
+	const currentDate = moment().format().split('T')[0];
+
+	expect(response.body[0].title).toEqual(projectItemConfig.testTitle);
+	expect(response.body[0].category).toEqual(projectItemConfig.categoryTask);
+	expect(response.body[0].description).toEqual(
+		projectItemConfig.testDescription
+	);
+	expect(response.body[0].dateCreated.split('T')[0]).toEqual(currentDate);
+	expect(response.body[0].dateUpdated.split('T')[0]).toEqual(currentDate);
+	expect(response.body[0].dateFinished).toBeFalsy();
+	expect(response.body[0].projectId).toEqual(projectIdUserA);
+	expect(response.body[0].id).toBeDefined();
+	expect(response.body[0].comments.length).toEqual(0);
+
+	expect(response.body._id).toBeUndefined();
+	expect(response.body.__v).toBeUndefined();
+});
+
 it("should not return items that belong to user with different ID than the project owner's ID", async () => {
 	await createProjectItem(
 		cookieUserA,
 		projectItemConfig.categoryIssue,
-		projectId
+		projectIdUserA
 	);
 	await createProjectItem(
 		cookieUserA,
 		projectItemConfig.categoryIssue,
-		projectId
+		projectIdUserA
 	);
 	await createProjectItem(
 		cookieUserB,
 		projectItemConfig.categoryTask,
-		projectId
+		projectIdUserB
 	);
 
 	let response = await request(app)
-		.get(`${projectConfig.baseProjectUrl}/${projectId}/items`)
+		.get(`${projectConfig.baseProjectUrl}/${projectIdUserA}/items`)
 		.set('Cookie', cookieUserA)
 		.expect(200);
 
@@ -96,7 +130,7 @@ it("should not return items that belong to user with different ID than the proje
 	expect(response.body[1].category).toEqual(projectItemConfig.categoryIssue);
 
 	response = await request(app)
-		.get(`${projectConfig.baseProjectUrl}/${projectId}/items`)
+		.get(`${projectConfig.baseProjectUrl}/${projectIdUserB}/items`)
 		.set('Cookie', cookieUserB)
 		.expect(200);
 
@@ -108,22 +142,22 @@ it('should return all project items that are tasks if query string param type is
 	await createProjectItem(
 		cookieUserA,
 		projectItemConfig.categoryIssue,
-		projectId
+		projectIdUserA
 	);
 	await createProjectItem(
 		cookieUserA,
 		projectItemConfig.categoryIssue,
-		projectId
+		projectIdUserA
 	);
 	await createProjectItem(
 		cookieUserA,
 		projectItemConfig.categoryTask,
-		projectId
+		projectIdUserA
 	);
 
 	const response = await request(app)
 		.get(
-			`${projectConfig.baseProjectUrl}/${projectId}/items?category=${projectItemConfig.categoryTask}`
+			`${projectConfig.baseProjectUrl}/${projectIdUserA}/items?category=${projectItemConfig.categoryTask}`
 		)
 		.set('Cookie', cookieUserA)
 		.expect(200);
@@ -136,22 +170,22 @@ it('should return all project items that are issues if query string param type i
 	await createProjectItem(
 		cookieUserA,
 		projectItemConfig.categoryIssue,
-		projectId
+		projectIdUserA
 	);
 	await createProjectItem(
 		cookieUserA,
 		projectItemConfig.categoryIssue,
-		projectId
+		projectIdUserA
 	);
 	await createProjectItem(
 		cookieUserA,
 		projectItemConfig.categoryTask,
-		projectId
+		projectIdUserA
 	);
 
 	const response = await request(app)
 		.get(
-			`${projectConfig.baseProjectUrl}/${projectId}/items?category=${projectItemConfig.categoryIssue}`
+			`${projectConfig.baseProjectUrl}/${projectIdUserA}/items?category=${projectItemConfig.categoryIssue}`
 		)
 		.set('Cookie', cookieUserA)
 		.expect(200);
@@ -163,7 +197,7 @@ it('should return all project items that are issues if query string param type i
 it('should return 422 if query string param type is set to something else than task or issue', async () => {
 	await request(app)
 		.get(
-			`${projectConfig.baseProjectUrl}/${projectId}/items?category=${projectItemConfig.invalidCategoryType}`
+			`${projectConfig.baseProjectUrl}/${projectIdUserA}/items?category=${projectItemConfig.invalidCategoryType}`
 		)
 		.set('Cookie', cookieUserA)
 		.expect(422);
@@ -172,7 +206,16 @@ it('should return 422 if query string param type is set to something else than t
 it('should return 403 if user is not signed in', async () => {
 	await request(app)
 		.get(
-			`${projectConfig.baseProjectUrl}/${projectId}/items?category=${projectItemConfig.categoryIssue}`
+			`${projectConfig.baseProjectUrl}/${projectIdUserA}/items?category=${projectItemConfig.categoryIssue}`
 		)
 		.expect(403);
+});
+
+it('should return 404 if project with specified ID does not exist', async () => {
+	await request(app)
+		.get(
+			`${projectConfig.baseProjectUrl}/${projectConfig.testProjectId}/items?category=${projectItemConfig.categoryIssue}`
+		)
+		.set('Cookie', cookieUserA)
+		.expect(404);
 });
--- backend/src/routes/project-items/fetch-project-items.ts
+++ backend/src/routes/project-items/fetch-project-items.ts
@@ -1,9 +1,57 @@
 import { Router, Request, Response } from 'express';
 
+import { requireAuth } from '../../middleware/require-auth';
+import { currentUser } from '../../middleware/current-user';
+import {
+	ProjectItem,
+	ProjectItemCategory,
+	ProjectItemDocument,
+} from '../../models/project-item';
+import { Project } from '../../models/project';
+import { CustomRequestValidationError } from '../../errors/custom-request-validation-error';
+import { NotFoundError } from '../../errors/not-found-error';
+import { serializeProjectItem } from '../../services/serialize-project-item';
+
 const router = Router();
 
-router.get(`/projects/:projectId/items`, (req: Request, res: Response) => {
-	return res.send('items');
-});
+router.get(
+	`/projects/:projectId/items`,
+	currentUser,
+	requireAuth,
+	async (req: Request, res: Response) => {
+		const { projectId } = req.params;
+		const { category } = req.query;
+
+		// category should be either undefined or one of the valid ones
+		if (
+			category !== ProjectItemCategory.Issue &&
+			category !== ProjectItemCategory.Task &&
+			category !== undefined
+		) {
+			throw new CustomRequestValidationError(
+				`invalid category, expected ${ProjectItemCategory.Issue} or ${ProjectItemCategory.Task}`
+			);
+		}
+
+		// project owner's ID must match the current user's ID
+		const project = await Project.findById(projectId);
+
+		if (!project) {
+			throw new NotFoundError(
+				`Project with ID ${projectId} does not exist`
+			);
+		}
+
+		// don't pass category to search criteria if it is undefined
+		let projectItems: ProjectItemDocument[];
+		if (category) {
+			projectItems = await ProjectItem.find({ category, projectId });
+		} else {
+			projectItems = await ProjectItem.find({ projectId });
+		}
+
+		return res.send(projectItems.map(serializeProjectItem));
+	}
+);
 
 export { router as fetchProjectItemsRouter };
